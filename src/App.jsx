7484fdfba956020c850d2a9c922d817615158a19
Abhinav jain <abhinavjain2022@vitbhopal.ac.in>
2024-10-20 19:15:23 +0530

    Added a About Us Page
    Added about us page and its routing
    The page is basic and its content can be modified according to your wish!

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Dashboardpage from './pages/Dashboardpage';
 import Dashboard from './components/Dashboard'; 
 import ProfilePage from './components/ProfilePage'; 
 import ContactPage from './pages/ContactPage';
+import AboutUs from './pages/AboutUs';
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         <Route path="/repo" element={<Repopage />} />
         <Route path="/dashboard" element={<Dashboardpage></Dashboardpage>} />
         <Route path='/contact' element={<ContactPage></ContactPage>}/>
+        <Route path="/about" element={<AboutUs></AboutUs>}/>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
